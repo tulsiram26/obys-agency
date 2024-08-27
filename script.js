566944b50319d8f8b17fa7c334c12cca30d673c8
tulsi <gavdetulsiram82@gmail.com>
2024-08-27 23:55:57 +0530

    footerdone

--- script.js
+++ script.js
@@ -1,70 +1,157 @@
-function loadingAnimation(){
-    var tl = gsap.timeline()
-tl.from(".line h1",{
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+ 
+function loader(){
+    var tl = gsap.timeline();
+tl.from(".line h1 ,.line h2",{
     y:150,
-    stagger:0.25,
-    duration:0.6,
-    delay:0.5,      
-});
-tl.from("#line1-part1",{
-    opacity:0,
-    onStart: function(){
-        var h5timer = document.querySelector("#line1-part1 h5")
-        var grow = 0
-        setInterval(function(){
-            if(grow<100){
-                h5timer.innerHTML = grow++
-            }else{
-        h5timer.innerHTML = grow
-            }
-        },33)
-            },
-});
-
-tl.to(".line h2",{
-    animationName:"anime",
-    opacity:1,
-})
-
+    Duration:0.6,
+    delay:0.5,
+    stagger:0.2
+}) 
 tl.to("#loader",{
     opacity:0,
-    duration:0.2,
-    delay:4,
-});
-
+    Duration:0.5,
+    delay:3,
+})
+gsap.from(".line1-part1",{
+    opacity:0,
+    delay:1,
+    onStart:function(){
+        var h5timer = document.querySelector(".line1-part1 h6")
+var count= 0;
+var int = setInterval(function(){
+    if(count===100){
+        clearInterval(int)
+    }
+    else{
+        count++;
+        h5timer.textContent=count;
+    }
+},30)
+    }})
 tl.from("#page1",{
-    dealy:0.2,
-    y:1600,
+    y:150,
     opacity:0,
-    duration:0.5,
-    ease:Power4,
+    delay:0.2,
+   ease:Power4
+
 })
 tl.to("#loader",{
-    dispaly:"none",
+    display:"none"
 })
 tl.from("#nav",{
-   opacit:0,
+    opacity:0
 })
-tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
-    y:140,
-    stagger:0.2,
-})
-};
+tl.from(".hero h1,.hero h2",{
+    y:500,
+    opacity:0,
+    stagger:0.1,
+    Duration:0.2 
+},"-=1.2")
 
-
-loadingAnimation();
-
-function cursorAnimation(){
-    document.addEventListener("mousemove",function(dets){
-        gsap.to("#crsr",{
-            left:dets.x,
-            top:dets.y,
-        })
-    })
-    
-    // magnet mouse attractor
-   Shery.makeMagnet("#nav-part2 h4")
-    
 }
 
-cursorAnimation();
+function magnethover(){
+    //cursor circle
+    Shery.mouseFollower({
+        skew:true,
+        ease:"cubic-bezier(0.23,1,0.320,1)",
+        Duration:1,
+    });
+
+    //To make nav bar magnetic
+    Shery.makeMagnet("#nav-part2 h4")
+
+    //This is for video play pause and mouse follower in video container
+    var videocontainer =document.querySelector("#video-container")
+    var videocursor = document.querySelector("#video-cursor");
+    var videocover = document.querySelector("#video-cover")
+    var video = document.querySelector("#video-container video")
+    videocontainer.addEventListener("mouseenter",function(){
+        gsap.to(".mousefollower",{
+            opacity:0
+        })
+        videocontainer.addEventListener("mousemove",function(p){
+           gsap.to("#video-cursor",{
+            left:p.x -470,
+            top:p.y - 200
+           })
+        })
+    })
+    videocontainer.addEventListener("mouseleave",function(){
+        gsap.to(".mousefollower",{
+            opacity:1
+        })
+        gsap.to("#video-cursor",{
+            left:"70%",
+            top:"-15%"
+        })
+    })
+   
+        var flag = 0;
+    videocursor.addEventListener("click",function(){
+           if(flag===0){
+           videocover.style.opacity=0;
+           video.play();
+           videocursor.innerHTML=`<i class="ri-pause-mini-line"></i>`
+           gsap.to("#video-cursor",{
+            scale:0.5,
+           })
+           flag = 1;
+           }
+           else{
+            videocover.style.opacity=1;
+            video.pause();
+            videocursor.innerHTML=`<i class="ri-play-fill"></i>`
+            gsap.to("#video-cursor",{
+             scale:1,
+            })
+            flag = 0;
+           }
+    })
+}
+
+function sheryanimation(){
+    Shery.imageEffect(".img-div",{
+        style:5,
+        config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":"9999999","range":[-9999999,9999999]},"aspect":{"value":0.7272749932567818},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1.24,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.76,"range":[0,10]},"metaball":{"value":0.41,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+        gooey:true
+    }) 
+}
+
+magnethover();
+loader();
+sheryanimation();
+locomotive();
